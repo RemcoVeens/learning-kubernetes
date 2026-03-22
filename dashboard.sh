6684a0b09c130ae550c41293daa8697b83c4833c
RemcoVeens <remco@track32.nl>
2026-03-22 14:35:00 +0100

    gateways

--- dashboard.sh
+++ dashboard.sh
@@ -5,10 +5,12 @@ case "$1" in
 	minikube start --extra-config="apiserver.cors-allowed-origins=['http://boot.dev']"
 	minikube dashboard --port=63840 &
 	kubectl proxy &
+	minikube tunnel --bind-address="127.0.0.1" -c &
         ;;
     stop)
         echo "Stopping minikube"
 	minikube stop
+	sudo pkill -f "minikube tunnel"
         ;;
     *)
         echo "Usage: $0 {start|stop}"
